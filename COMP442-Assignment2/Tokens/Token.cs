2817ce3b74678a7a6d0e1b57d4f119ef88f270ad
Michael B <netopya@netopyaplanet.com>
2017-02-12 17:01:29 -0500

    Started token improvements
    -Also some more namespace changes

---
+++ COMP442-Assignment2/Tokens/Token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP442_Assignment2.Tokens
+{
+    class Token
+    {
+        private string name;
+
+        public Token(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+
+    }
+
+
+}
