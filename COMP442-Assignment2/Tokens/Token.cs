ba1a8fccbb9e90ab9604f5bb56bba4d16c77e006
Michael B <netopya@netopyaplanet.com>
2017-02-12 17:15:23 -0500

    Created token list

--- COMP442-Assignment2/Tokens/Token.cs
+++ COMP442-Assignment2/Tokens/Token.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace COMP442_Assignment2.Tokens
 {
-    class Token
+    public class Token
     {
         private string name;
 
@@ -19,9 +19,41 @@ namespace COMP442_Assignment2.Tokens
         {
             return name;
         }
-
-
     }
 
+    public static class TokenList
+    {
+        public static Token Error = new Token("Error");
+        public static Token Asterisk = new Token("Asterisk");
+        public static Token LineComment = new Token("Line comment");
+        public static Token BlockComment = new Token("Block comment");
+        public static Token Slash = new Token("Slash");
+
+        public static Token OpenParanthesis = new Token("Open parenthesis");
+        public static Token CloseParanthesis = new Token("Close parenthesis");
+        public static Token OpenCurlyBracket = new Token("Open curly bracket");
+        public static Token CloseCurlyBracket = new Token("Close curly bracket");
+        public static Token OpenSquareBracket = new Token("Open square bracket");
+        public static Token CloseSquareBracket = new Token("Close square bracket");
+
+        public static Token GreaterThanOrEqual = new Token("Greater than or equal");
+        public static Token GreaterThan = new Token("Greater than");
+        public static Token LessThanOrEqual = new Token("Less than or equal");
+        public static Token NotEqual = new Token("Not equal");
+        public static Token LessThan = new Token("Less than");
+        public static Token DoubleEquals = new Token("Double equals");
+        public static Token EqualsToken = new Token("Equals");
+
+        public static Token Period = new Token("Period");
+        public static Token SemiColon = new Token("Semi-colon");
+        public static Token Comma = new Token("Comma");
+        public static Token Plus = new Token("Plus");
+        public static Token Minus = new Token("Minus");
+
+        public static Token Float = new Token("Float");
+        public static Token Integer = new Token("Integer");
+
+        public static Token Identifier = new Token("Identifier");
+    }
 
 }
