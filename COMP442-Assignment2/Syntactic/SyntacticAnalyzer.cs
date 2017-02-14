11abf78e332011716ca36200fe456ded1222436f
Netopya <netopya@netopyaplanet.com>
2017-02-14 14:10:07 -0500

    fixed bug with statement

--- COMP442-Assignment2/Syntactic/SyntacticAnalyzer.cs
+++ COMP442-Assignment2/Syntactic/SyntacticAnalyzer.cs
@@ -25,10 +25,10 @@ namespace COMP442_Assignment2.Syntactic
             Production funcList = new Production("funcList", new List<Token> { TokenList.Epsilon, TokenList.IntRes, TokenList.FloatRes, TokenList.Identifier}, new List<Token> { TokenList.EndOfProgram});
             Production funcDef = new Production("funcDef", new List<Token> { TokenList.OpenParanthesis}, new List<Token> { TokenList.IntRes, TokenList.FloatRes, TokenList.Identifier, TokenList.EndOfProgram, TokenList.CloseCurlyBracket});
             Production funcBody = new Production("funcBody", new List<Token> { TokenList.OpenCurlyBracket}, new List<Token> { TokenList.SemiColon});
-            Production funcBodyList = new Production("funcBodyList", new List<Token> { TokenList.IntRes, TokenList.FloatRes,  TokenList.Identifier, TokenList.Epsilon}, new List<Token> { TokenList.CloseCurlyBracket});
+            Production funcBodyList = new Production("funcBodyList", new List<Token> { TokenList.IntRes, TokenList.FloatRes,  TokenList.Identifier, TokenList.Epsilon, TokenList.If, TokenList.For, TokenList.Get, TokenList.Put, TokenList.Return}, new List<Token> { TokenList.CloseCurlyBracket});
             Production idtypeFuncBodyList = new Production("idtypeFuncBodyList", new List<Token> { TokenList.Identifier, TokenList.If, TokenList.For, TokenList.Get, TokenList.Put, TokenList.Return, TokenList.Epsilon, TokenList.OpenSquareBracket, TokenList.Period, TokenList.EqualsToken}, new List<Token> { TokenList.CloseCurlyBracket});
             Production ntypeFuncBodyList = new Production("ntypeFuncBodyList", new List<Token> { TokenList.Identifier}, new List<Token> { TokenList.CloseCurlyBracket});
-            Production varDecl = new Production("varDecl", new List<Token> { TokenList.SemiColon, TokenList.Epsilon, TokenList.OpenSquareBracket}, new List<Token> { TokenList.IntRes, TokenList.FloatRes, TokenList.Identifier, TokenList.CloseCurlyBracket});
+            Production varDecl = new Production("varDecl", new List<Token> { TokenList.SemiColon, TokenList.Epsilon, TokenList.OpenSquareBracket}, new List<Token> { TokenList.IntRes, TokenList.FloatRes, TokenList.Identifier, TokenList.CloseCurlyBracket, TokenList.If, TokenList.For, TokenList.Get, TokenList.Put, TokenList.Return});
             Production arraySizeList = new Production("arraySizeList", new List<Token> { TokenList.Epsilon, TokenList.OpenSquareBracket}, new List<Token> { TokenList.Comma, TokenList.SemiColon, TokenList.CloseParanthesis});
             Production statement = new Production("statement", new List<Token> { TokenList.If, TokenList.For, TokenList.Get, TokenList.Put,TokenList.Return}, new List<Token> { TokenList.Identifier, TokenList.If,TokenList.For, TokenList.Get, TokenList.Put, TokenList.Return, TokenList.IntRes,TokenList.FloatRes, TokenList.CloseCurlyBracket, TokenList.Else, TokenList.SemiColon});
             Production assignStat = new Production("assignStat", new List<Token> { TokenList.Epsilon, TokenList.OpenSquareBracket, TokenList.Period, TokenList.EqualsToken}, new List<Token> { TokenList.SemiColon, TokenList.CloseParanthesis});
@@ -88,6 +88,7 @@ namespace COMP442_Assignment2.Syntactic
             Rule r13 = new Rule(funcBodyList, new List<IProduceable> { TokenList.IntRes, ntypeFuncBodyList}); //funcBodyList -> intRes ntypeFuncBodyList
             Rule r14 = new Rule(funcBodyList, new List<IProduceable> { TokenList.FloatRes, ntypeFuncBodyList }); //funcBodyList -> floatRes ntypeFuncBodyList
             Rule r15 = new Rule(funcBodyList, new List<IProduceable> { TokenList.Identifier, idtypeFuncBodyList }); //funcBodyList -> id idtypeFuncBodyList
+            Rule r15a = new Rule(funcBodyList, new List<IProduceable> { statement, funcBodyList}); // funcBodyList -> statement funcBodyList
             Rule r16 = new Rule(funcBodyList); // funcBodyList -> EPSILON
             Rule r17 = new Rule(idtypeFuncBodyList, new List<IProduceable> { TokenList.Identifier, varDecl, funcBodyList}); //idtypeFuncBodyList -> id varDecl funcBodyList
             Rule r18 = new Rule(idtypeFuncBodyList, new List<IProduceable> { statement, funcBodyList}); //idtypeFuncBodyList -> statement funcBodyList
@@ -182,7 +183,7 @@ namespace COMP442_Assignment2.Syntactic
 
             // I didn't write this out by hand, trust me
             rules.AddRange(new List<Rule> { r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14,
-                r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32,
+                r15, r15a, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32,
                 r33, r34, r35, r36, r37, r38, r39, r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, r50,
                 r51, r52, r53, r54, r55, r56, r57, r58, r59, r60, r61, r62, r63, r64, r65, r66, r67, r68,
                 r69, r70, r71, r72, r73, r74, r75, r76, r77, r78, r79, r80, r81, r82, r83, r84, r85, r86, r87, r88, r89, r90, r91 });
